75adcc57adc631ee8cf623c394bc1748bbc66020
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2023-08-29 20:08:06 +0400

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
 
+let changeСolorInterval = null;
+
+buttonStart.addEventListener('click', startColorChange);
+buttonStop.addEventListener('click', stopColorChange);
+
+
+function startColorChange() {
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+    changeСolorInterval = setInterval(changeBackgroundColor, 1000);
+};
+
+function stopColorChange() {
+    buttonStart.disabled = false;
+    buttonStop.disabled = true;
+    clearInterval(changeСolorInterval);
+
+};
+ function changeBackgroundColor() {
+      const randomColor = getRandomHexColor();
+      document.body.style.backgroundColor = randomColor;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
